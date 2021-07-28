5f258cbcafca65f9f14d90eae4588ab802f6900b
jhjang <jhjang@10.11.52.125>
2021-07-28 17:19:47 +0900

    버튼 클릭 이벤트 추가

--- ComposeApp/app/src/main/java/com/example/composeapp/MainActivity.kt
+++ ComposeApp/app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -3,23 +3,27 @@ package com.example.composeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.composeapp.ui.theme.ComposeAppTheme
 import com.example.composeapp.ui.theme.Purple200
+import com.example.composeapp.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MyComposeApp {
-                Greeting("Android")
+                TestContent()
             }
         }
     }
@@ -37,9 +41,38 @@ fun MyComposeApp(content : @Composable () -> Unit) {
 }
 
 @Composable
+fun TestContent(names : List<String> = listOf("test", "meet", "nice", "Android", "hola")) {
+    Column {
+        for (name in names){
+            Greeting(name)
+//            Text(text = name, modifier = Modifier.padding(10.dp), textAlign = TextAlign.Center)
+            Divider(color = Teal200)
+        }
+        Divider(color = Color.Black, thickness = 3.dp)
+        CounterButton()
+    }
+}
+
+@Composable
+fun CounterButton() {
+    val counter = remember {
+        mutableStateOf(0)
+    }
+
+    Button(onClick = {
+        counter.value++
+    }) {
+        Text(text = "Button Click Count = ${counter.value}")
+    }
+}
+
+@Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
-    Text(text = "Hllo $name!")
+    Column {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+        Divider(color = Teal200)
+        Text(text = "Hllo $name!")
+    }
 }
 
 @Preview(showBackground = true)
