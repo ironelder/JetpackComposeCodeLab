0a5cdd519a478a826bd7d589cb3977172784edbe
jhjang <jhjang@10.11.52.125>
2021-07-28 16:44:21 +0900

    Jetpack Compose App init

---
+++ ComposeApp/app/src/main/java/com/example/composeapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.composeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composeapp.ui.theme.ComposeAppTheme
+import com.example.composeapp.ui.theme.Purple200
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyComposeApp()
+        }
+    }
+}
+
+@Composable
+fun MyComposeApp() {
+    ComposeAppTheme {
+        // A surface container using the 'background' color from the theme
+        //MaterialTheme.colors.background
+        Surface(color = Purple200) {
+            Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    Text(text = "Hllo $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyComposeApp()
+}
